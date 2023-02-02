3252ef2861d42eef0afcf1193ead415ca7e8eda2
dunzolo <rossidavide108@gmail.com>
2023-02-02 18:02:49 +0100

    add methods addTask button click

--- js/script.js
+++ js/script.js
@@ -8,6 +8,22 @@ createApp({
             task: '',
         }
     },
+    methods: {
+        //aggiungo un nuovo elemento
+        addTask() {
+            const data = {
+                task: this.task,
+                done: false,
+            }
+
+            axios.post(this.api_url, data, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }).then((response) => {
+                this.task = '';
+                this.array_list = response.data;
+            })
+        },
+    },
     created() {
         axios.get(this.api_url).then((response) => {
             this.array_list = response.data;
--- server.php
+++ server.php
@@ -5,6 +5,25 @@
     //2 - converto la stringa in array associativo
     $array_list = json_decode($string, true);
 
+    //2.1 - aggiungo un nuovo elemento all'array associativo
+    if(isset($_POST['task'])){
+        //creo i dati da passare all'array, recuperando il dato in input
+        $task = $_POST['task'];
+        $done = false;
+        
+        //creo l'array
+        $array_task = [
+            "task" => $task,
+            "done"  => $done,
+        ];
+
+        //aggiungo l'array appena creato a quelli esistenti del file json
+        $array_list[] = $array_task;
+        
+        //scrivo il dato all'interno del file json
+        file_put_contents('todo-list.json', json_encode($array_list, JSON_PRETTY_PRINT));
+    }
+
     //3 - codifico in formato json l'array
     header('Content-Type: application/json');
     echo json_encode($array_list);
